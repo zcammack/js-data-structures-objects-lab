d9e2d55dc3c1c8087323e9d02a42edecbfdc1a13
zcammack <zack.cammack@gmail.com>
2021-02-13 18:40:04 -0600

    commit tree 756e50931908a4553873bcffe343ec2809de0a20

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+let driver = { foo: "bar" };
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+
+    const newDriver = { ...driver };
+
+    newDriver[key] = value;
+
+    return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+
+    driver[key] = value;
+
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+
+    const newDriver = { driver };
+
+    delete newDriver.key;
+
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+
+    delete driver[key];
+
+    return driver;
+}
